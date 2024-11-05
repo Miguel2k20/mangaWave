da3fdc1089f5bcbf22724d0387eeb27aed36c67d
Miguel <joao.miguel@inbraep.com.br>
2024-11-05 11:03:20 -0300

    front-testes

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
 import customtkinter
 from MangaApiClient import MangaApiClient
 
-def button_callback():
-    print("button clicked")
-
 app = customtkinter.CTk()
-app.geometry("500x400")
+app.geometry("650x400")
+app.title("MangaWave")
 customtkinter.set_appearance_mode("dark")
+
+app.minsize(950, 500)
 
 def getInputValue():
     print(MangaApiClient.getManga(entry.get()))
 
-entry = customtkinter.CTkEntry(master=app, placeholder_text="Que mangá você está buscando?", width=300) 
-btn = customtkinter.CTkButton(master=app, text="Buscar", command=getInputValue)
+title = customtkinter.CTkLabel(
+    master=app, 
+    text="O que vamos ler hoje?", 
+    font=("arial", 20)
+)
 
-entry.pack(anchor="s", expand=True, pady=10)
-btn.pack(anchor="n", expand=True)
+entry = customtkinter.CTkEntry(
+    master=app, 
+    placeholder_text="Que mangá você está buscando?", 
+    width=450,
+    font=("arial", 15)
+)
+
+btn = customtkinter.CTkButton(
+    master=app, 
+    text="Buscar", 
+    command=getInputValue
+)
+
+mangaResults =  customtkinter.CTkScrollableFrame(
+    master=app, 
+    width=900,
+    height = 300,
+    fg_color="#fff"
+)
+
+
+title.pack(anchor="center", pady=(20, 5))  # Espaçamento maior em cima, menor embaixo
+entry.pack(anchor="center", pady=(5, 5))   # Espaçamento pequeno em cima e embaixo
+btn.pack(anchor="center", pady=(5, 20))  
+mangaResults.pack(expand=True)
 
 app.mainloop()
